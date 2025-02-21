a5fd44627c558504bee70217c35ed9986da1fd02
TonyKujo <131020164+TonyKujo@users.noreply.github.com>
2025-02-21 19:14:44 +0300

    #1.2 Написал DbContext + добавил модели
    Написал DbContext, Добавил 2 модели: Юзера и Задачу.

--- TaskTrackerBackend.DAL/AppDbContext.cs
+++ TaskTrackerBackend.DAL/AppDbContext.cs
@@ -1,5 +1,27 @@
 ﻿namespace TaskTrackerBackend.DAL;
-
-internal class AppDbContext()
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TaskTrackerBackend.DAL.Models;
+public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Task> Tasks { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(builder =>
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Login).HasMaxLength(10);
+            builder.Property(x => x.Password).HasMaxLength(10);
+        });
+
+        modelBuilder.Entity<Task>(builder =>
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId);
+            builder.Property(x => x.Title).IsRequired().HasMaxLength(128);
+            builder.Property(x => x.Description).HasMaxLength(300);
+        });
+    }
 }
---
+++ TaskTrackerBackend.DAL/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskTrackerBackend.DAL.Models;
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+    public DateTime Deadline { get; set; }
+    public TaskStatus Status { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
+
+public enum TaskStatus
+{
+    Running,
+    Success,
+    Failed,
+}
